ee790fa7c841f087195e305a80e99a06040116a0
denis0bej <denis.bejan.topse@gmail.com>
2024-11-15 09:40:33 +0200

    am bagat scriptul de ora trecuta

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cat /etc/hosts | while read ip dom
+do
+	if [ "$dom" != localhost ] && [[ ! $dom == *`hostname`* ]] && [ -n "$dom" ]; then
+		if [ "$ip" = "#" ]; then
+			break
+		fi
+		gasit=0
+		if [ -n "$dom" ]; then
+			nslookup $dom 8.8.8.8 | while read a text; do
+				if [ "$text" = "$ip" ]; then
+					gasit=1
+					touch gasit
+				fi
+			done <<< "$(nslookup "$dom" 8.8.8.8)"
+		fi
+		if [ -e gasit ]; then
+			rm gasit
+		else
+			echo "Bogus IP for $dom in /etc/hosts!"
+		fi
+	fi
+done
